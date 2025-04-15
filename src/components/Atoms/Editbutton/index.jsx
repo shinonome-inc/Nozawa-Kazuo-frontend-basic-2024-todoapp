781f9564eafbada5376476286f55e8858fe401fd
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-04-15 20:13:39 +0900

    format_修正

--- src/components/Atoms/Editbutton/index.jsx
+++ src/components/Atoms/Editbutton/index.jsx
@@ -7,23 +7,23 @@ import FONTFAMILY from "../../../variables/font_family.js";
 import pencil from "../../../assets/svg/pencil.svg";
 
 export const EditButton = () => {
-    return (
-        <StyledButton onClick= {() => console.log('click')}>
-            <img src={pencil}/>
-        </StyledButton>)
-        }
+  return (
+    <StyledButton onClick={() => console.log("click")}>
+      <img src={pencil} />
+    </StyledButton>
+  );
+};
 
 const StyledButton = styled.button`
-    width: 20px;
-    height: 20px;
-    display: flex; /*イラストを枠の真ん中に置く*/
-    border: none;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent;/* 透明にすることで背景が崩れないようにする */  
-    &:hover {
+  width: 20px;
+  height: 20px;
+  display: flex; /*イラストを枠の真ん中に置く*/
+  border: none;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent; /* 透明にすることで背景が崩れないようにする */
+  &:hover {
     background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
-    border-radius: 50%;/* 正方形を円にすることが出来る */
-    }
-
+    border-radius: 50%; /* 正方形を円にすることが出来る */
+  }
 `;
