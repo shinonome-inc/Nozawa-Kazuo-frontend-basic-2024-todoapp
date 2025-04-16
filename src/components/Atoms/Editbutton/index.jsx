ab109783de9f4c1756f53a737304c38857752401
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-04-16 12:58:34 +0900

    EditButton-修正1

--- src/components/Atoms/Editbutton/index.jsx
+++ src/components/Atoms/Editbutton/index.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import BREAKPOINT from "../../../variables/breakpoint.js";
 import COLOR from "../../../variables/color.js";
-import TEXT from "../../../variables/texts.js";
-import FONTFAMILY from "../../../variables/font_family.js";
 import pencil from "../../../assets/svg/pencil.svg";
 
-export const EditButton = () => {
+export const EditButton = (onClick) => {
   return (
-    <StyledButton onClick={() => console.log("click")}>
-      <img src={pencil} />
+    <StyledButton onClick={onClick}>/*ここの{}にconstを使うときに処理を足すことで機能を付けられる */
+      <img src={pencil} alt="image"/>
     </StyledButton>
   );
 };
