d24012b1a2e933a98ce34422dbede7b06fddd92f
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-04-19 02:05:24 +0900

    addtaskbutton修正

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -1,43 +1,39 @@
-import Plus from "../../../assets/svg/plus.svg"
-import styled from "styled-components"
-import TEXT from "../../../variables/texts.js"
-import FONTFAMILY from "../../../variables/font_family.js"
-import COLOR from "../../../variables/color.js"
+import Plus from "../../../assets/svg/plus.svg";
+import styled from "styled-components";
+import TEXT from "../../../variables/texts.js";
+import FONTFAMILY from "../../../variables/font_family.js";
+import COLOR from "../../../variables/color.js";
 
-export const AddTaskButton = () =>{
-    return(
-        <StyledButton>
-            <StyledImage src={Plus}/>
-            <StyledText>タスクを追加</StyledText>
-        </StyledButton>
-    )
-
-}
+export const AddTaskButton = ({onClick}) => {
+  return (
+    <StyledButton onClick={(onClick)}>
+      <StyledImage src={Plus} />
+      <StyledText>タスクを追加</StyledText>
+    </StyledButton>
+  );
+};
 
 const StyledButton = styled.button`
-    display: flex;
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    ${TEXT.S}
-    font-family: ${FONTFAMILY.NOTO_SANS};
-    color: ${COLOR.GREEN};
-    gap: 10px;
-    padding: 2px 6px;
-    &:hover {
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  ${TEXT.S}
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  color: ${COLOR.GREEN};
+  gap: 10px;
+  padding: 2px 6px;
+  &:hover {
     background-color: ${COLOR.GREEN_TRANSLUCENT};
     border-radius: 12px;
-    }
+  }
 `;
 const StyledImage = styled.img`
-    width: 20px;
-    height: 20px;
-    
-    
+  width: 20px;
+  height: 20px;
 `;
 const StyledText = styled.p`
-    margin: 0;
-    letter-spacing: 0;
-    font-weight: 500;
-    line-height: 20px;
+  margin: 0;
+  letter-spacing: 0;
+  font-weight: 500;
 `;
