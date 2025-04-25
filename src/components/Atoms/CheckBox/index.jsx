07fa91fd6c42a3ad3d133408d5ad2b0b1688da37
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-04-25 12:32:35 +0900

    prettier format改善

--- src/components/Atoms/CheckBox/index.jsx
+++ src/components/Atoms/CheckBox/index.jsx
@@ -3,30 +3,28 @@ import CHECK from "../../../assets/svg/check.svg";
 import COLOR from "../../../variables/color.js";
 import React from "react";
 
-export const CheckBox = ({onClick}) => {
-    return(
-        <StyledBox onClick={(onClick)}>
-            <StyledImg src = {CHECK} alt="check"/>
-        </StyledBox>
-
-    )
-}
-
+export const CheckBox = ({ onClick }) => {
+  return (
+    <StyledBox onClick={onClick}>
+      <StyledImg src={CHECK} alt="check" />
+    </StyledBox>
+  );
+};
 
 const StyledBox = styled.button`
-    display: flex;
-    width: 20px;
-    height: 20px;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent; 
-    border: 2px solid ${COLOR.LIGHT_GRAY};
+  display: flex;
+  width: 20px;
+  height: 20px;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
 `;
 const StyledImg = styled.img`
-    width: 16px;
-    height: 16px;
-    opacity: 0;
-    &:hover  {
+  width: 16px;
+  height: 16px;
+  opacity: 0;
+  &:hover {
     opacity: 1;
-    }
+  }
 `;
