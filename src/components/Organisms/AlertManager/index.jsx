5bfb4c8869066c4567a568b071d1a48a2d355fa9
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-05-26 21:09:50 +0900

    Alert 途中

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -4,7 +4,9 @@ import { useAlertHandlerContext } from "path/to/contexts/alert_handler";
 /* コンポーネント関数内 */
 //Contextを取得
 const AlertHandlerContext = useAlertHandlerContext();
-
+setTimeout(() => {
+  (false);
+}, 3000);
 //AlertHandlerContextから直接値が取り出せる
 console.log(AlertHandlerContext.visible);
 AlertHandlerContext.setAlert("message"); //Alertの表示
