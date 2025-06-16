d15836eeb0accf9f7996d620dd0c284b44a4a8af
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2025-06-16 12:09:40 +0900

    MainPage機能　修正

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MainPages from "./components/Pages/MainPages/index";
+import MainPages from "./components/Pages/MainPages/index"
 import "./style.css";
 
 ReactDOM.render(
